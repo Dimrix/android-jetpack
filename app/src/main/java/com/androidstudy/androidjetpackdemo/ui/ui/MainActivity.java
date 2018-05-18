88f4e0c35b653a76459cbeef5eaffb8349c81c51
Juma Allan <allanjuma@gmail.com>
2018-05-18 11:08:09 +0300

    Added Java 8 support and Butterknife

--- app/src/main/java/com/androidstudy/androidjetpackdemo/ui/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/androidjetpackdemo/ui/ui/MainActivity.java
@@ -1,11 +1,11 @@
 package com.androidstudy.androidjetpackdemo.ui.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.androidstudy.androidjetpackdemo.R;
+import com.androidstudy.androidjetpackdemo.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
