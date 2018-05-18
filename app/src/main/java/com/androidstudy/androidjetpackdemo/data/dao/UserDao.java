0275439b6e38857046e02288ed2057423209cce9
Juma Allan <allanjuma@gmail.com>
2018-05-18 16:08:49 +0300

    Added Database & ViewModel

--- app/src/main/java/com/androidstudy/androidjetpackdemo/data/dao/UserDao.java
+++ app/src/main/java/com/androidstudy/androidjetpackdemo/data/dao/UserDao.java
@@ -1,6 +1,14 @@
 package com.androidstudy.androidjetpackdemo.data.dao;
 
+import android.arch.persistence.room.Insert;
+
 import com.androidstudy.androidjetpackdemo.data.model.User;
 
-public interface UserDao extends BaseDao<User> {
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+public interface UserDao {
+
+    //Insert User
+    @Insert(onConflict = REPLACE)
+    void addUser(User user);
 }
--- app/src/main/java/com/androidstudy/androidjetpackdemo/ui/viewmodel/MainViewModel.java
+++ app/src/main/java/com/androidstudy/androidjetpackdemo/ui/viewmodel/MainViewModel.java
@@ -1,6 +1,40 @@
 package com.androidstudy.androidjetpackdemo.ui.viewmodel;
 
-import android.arch.lifecycle.ViewModel;
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
-public class MainViewModel extends ViewModel {
+import com.androidstudy.androidjetpackdemo.data.Database;
+import com.androidstudy.androidjetpackdemo.data.model.User;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private Database appDatabase;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        appDatabase = Database.getDatabase(this.getApplication());
+    }
+
+    //Add New User
+    public void addUser(User user) {
+        new addAsyncTask(appDatabase).execute(user);
+    }
+
+    private static class addAsyncTask extends AsyncTask<User, Void, Void> {
+
+        private Database db;
+
+        addAsyncTask(Database appDatabase) {
+            db = appDatabase;
+        }
+
+        @Override
+        protected Void doInBackground(final User... params) {
+            db.userDao().addUser(params[0]);
+            return null;
+        }
+
+    }
 }
